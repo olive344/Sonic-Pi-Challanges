c59201266d8028974b47d8414dab6c1c09404b1f
olive344 <olivia_marquez@student.davincischools.org>
2025-02-20 13:50:29 -0800

    ChallangeE.rb

---
+++ ChallangeE.rb
@@ -0,0 +1,46 @@
+# Challenge E
+use_bpm 180
+use_synth :chipbass
+
+# VARIABLES TO STORE SAMPLES
+
+
+
+
+# INTRO
+play :e4
+sleep 0.5
+play :e4
+sleep 1
+play :e4
+sleep 1
+play :c4
+sleep 0.5
+play :e4
+sleep 1
+play :g4
+sleep 2
+play :g3
+sleep 2
+
+
+# CUSTOM MARIO SAMPLES HERE
+sample"C:/Users/olivia_marquez/Downloads/mario_sounds/mario_sounds/mario_sounds/coin.wav"
+
+
+sleep 2
+
+sample "C:/Users/olivia_marquez/Downloads/mario_sounds/mario_sounds/mario_sounds/pipe.wav"
+
+sleep 2
+
+sample "C:/Users/olivia_marquez/Downloads/mario_sounds/mario_sounds/mario_sounds/oneup.wav"
+
+sleep 2
+
+sample "C:/Users/olivia_marquez/Downloads/mario_sounds/mario_sounds/mario_sounds/mario_beat.wav"
+
+sleep 2
+
+sample "C:/Users/olivia_marquez/Documents/Audacity/Super Mario Bros. - Mushroom Sound Effect.wav"
+
