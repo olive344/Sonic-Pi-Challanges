8d2b96e8e1df8a03c1a6cfd3c45538aa216159b5
olive344 <olivia_marquez@student.davincischools.org>
2025-02-20 12:46:47 -0800

    ChallangeD.rb

---
+++ ChallangeD.rb
@@ -0,0 +1,126 @@
+use_bpm 136
+use_synth :piano
+define :measure5And7 do
+  play:B4
+  sleep 0.75
+  play:As4
+  sleep 0.25
+  play:Gs4
+  play:B4
+  sleep 0.75
+  play:Cs5
+  sleep 0.25
+  play:B4
+  sleep 1
+  play:Gs4
+  play:E5
+  sleep 1
+end
+
+define :measure2And8 do
+  play:E4
+  sleep 1
+  play:E5
+  play:Gs4
+  sleep 2
+  play:E5
+  play:Gs4
+  sleep 1
+end
+
+define :measure3And4And6 do
+  play :r
+  sleep 1
+  play:E5
+  play:Gs4
+  play:B4
+  sleep 1
+  play:B4
+  sleep 1
+  play:E5
+  play:Gs4
+  sleep 1
+end
+
+#DON'T CHANGE THIS LIVE LOOP
+live_loop:background_notes do
+  play:E3, amp: 0.25
+  sleep 1
+  play:B3, amp: 0.25
+  play:E4, amp: 0.25
+  sleep 1
+  play:B3, amp: 0.25
+  sleep 1
+  play:E4, amp: 0.25
+  sleep 1
+  
+  play:E3, amp: 0.5
+  sleep 1
+  play:B3, amp: 0.5
+  play:E4, amp: 0.5
+  sleep 1
+  play:B3, amp: 0.5
+  sleep 1
+  play:E4, amp: 0.5
+  sleep 1
+  
+  play:E3, amp: 0.75
+  sleep 1
+  play:B3, amp: 0.75
+  play:E4, amp: 0.75
+  sleep 1
+  play:B3, amp: 0.75
+  sleep 1
+  play:E4, amp: 0.75
+  sleep 1
+  
+  5.times do
+    play:E3, amp: 1
+    sleep 1
+    play:B3, amp: 1
+    play:E4, amp: 1
+    sleep 1
+    play:B3, amp: 1
+    sleep 1
+    play:E4, amp: 1
+    sleep 1
+  end
+  stop
+end
+live_loop:new_layer do
+  32.times do
+    sample :drum_heavy_kick
+    sleep 0.5
+    sample :bd_haus
+    sleep 0.5
+  end
+  stop
+end
+#OPTIMIZE THIS WITH FUNCTION
+# Measure 1
+play :r
+sleep 1
+play:E4
+play:Gs4
+sleep 2
+play:E5
+play:Gs4
+sleep 1
+
+measure2And8
+
+measure3And4And6
+
+measure3And4And6
+
+measure5And7
+
+measure3And4And6
+
+measure5And7
+
+measure2And8
+
+sample  :vinyl_scratch
+sleep 0.5
+sample :vinyl_scratch, sustain: 3
