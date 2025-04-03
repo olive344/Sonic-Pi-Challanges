d35e535867ba54ecdadffc094df2c65e9d712c5d
olive344 <olivia_marquez@student.davincischools.org>
2025-04-03 10:16:01 -0700

    Update Custom_song_code.rb

--- Custom_song_code.rb
+++ Custom_song_code.rb
@@ -1,71 +1,86 @@
 #custom_song_sample
 drum_sample= "C:/Users/olivia_marquez/Documents/Audacity/Drums Only – GATA ONLY - FloyyMenor ft. Cris MJ - (Drum Track).wav"
 
+
 use_bpm 100
 
+x=16
 define :bass do
-  sample :ambi_glass_hum, beat_stretch: 16
-  sleep 16
-  sample :ambi_glass_hum, beat_stretch: 8
-  sleep 8
-  sample :ambi_glass_hum, beat_stretch: 4
-  sleep 4
-  sample :ambi_glass_hum, beat_stretch: 2
-  sleep 2
+  sample :ambi_glass_hum, beat_stretch: x
+  sleep(x)
+  x = x/2
 end
-
-live_loop :bass do
-  bass
+live_loop:bass do
+  2.times do
+    2.times do
+      print(x)
+      bass
+      sleep 0.5
+    end
+    x=16
+  end
+  stop
 end
 
 sleep 10
 
 live_loop :drums do
-  sample drum_sample
-  sleep 6
+  3.times do
+    3.times do
+      sample drum_sample
+      sleep 6
+    end
+    stop
+  end
+  stop
 end
+
 
 
 sleep 15
-
-
-live_loop :piano do
-  use_synth :tri
-  play :gs3
-  sleep 0.5
-  play :cs4
-  sleep 0.5
-  play:cs4
-  sleep 0.5
-  play :as3
-  sleep 0.5
-  
-  play :as3
-  sleep 0.5
-  play :cs4
-  sleep 0.5
-  play :cs4
-  sleep 0.5
-  play :as3
-  sleep 0.5
-  
-  play :cs4
-  sleep 0.5
-  play :as3
-  sleep 0.5
-  play :cs4
-  sleep 0.5
-  play :as3
-  sleep 0.5
-  
-  play :cs4
-  sleep 0.5
-  play :as3
-  sleep 0.5
-  play :cs4
-  sleep 0.5
-  play :as3
+define :notes do |my_notes|
+  play my_notes
   sleep 0.5
 end
+
+3.times do
+  :piano
+  notes :gs3
+  notes :cs4
+  notes :cs4
+  notes :as3
+  
+  notes :as3
+  notes :cs4
+  notes :cs4
+  notes :as3
+  
+  
+  notes :cs4
+  notes :as3
+  notes :cs4
+  notes :as3
+  
+  
+  
+  notes :cs4
+  notes :as3
+  notes :cs4
+  notes :as3
+  sample :drum_bass_soft
+  sleep 0.5
+end
+
+5.times do
+  sample :drum_bass_soft
+  sleep 1
+end
+
+
+
+
+
+
+
 
 
