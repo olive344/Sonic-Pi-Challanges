d1ae90c5226bae7e0f472386aa918ae95a77ac65
olive344 <olivia_marquez@student.davincischools.org>
2025-04-10 15:45:34 -0700

    Custom_song_code.rb

--- Custom_song_code.rb
+++ Custom_song_code.rb
@@ -1,6 +1,6 @@
 #custom_song_sample
 drum_sample= "C:/Users/olivia_marquez/Documents/Audacity/Drums Only – GATA ONLY - FloyyMenor ft. Cris MJ - (Drum Track).wav"
-
+drum_beat_sample="C:/Users/olivia_marquez/Downloads/audiomass-output2.wav"
 
 use_bpm 100
 
@@ -44,41 +44,63 @@ define :notes do |my_notes|
 end
 
 3.times do
-  :piano
-  notes :gs3
-  notes :cs4
-  notes :cs4
-  notes :as3
-  
-  notes :as3
-  notes :cs4
-  notes :cs4
-  notes :as3
-  
-  
-  notes :cs4
-  notes :as3
-  notes :cs4
-  notes :as3
-  
-  
-  
-  notes :cs4
-  notes :as3
-  notes :cs4
-  notes :as3
-  sample :drum_bass_soft
-  sleep 0.5
+  with_fx :tremolo do
+    :piano
+    notes :gs3
+    notes :cs4
+    notes :cs4
+    notes :as3
+    
+    notes :as3
+    notes :cs4
+    notes :cs4
+    notes :as3
+    
+    
+    notes :cs4
+    notes :as3
+    notes :cs4
+    notes :as3
+    
+    
+    
+    notes :cs4
+    notes :as3
+    notes :cs4
+    notes :as3
+    sample :drum_bass_soft
+    sleep 0.5
+  end
 end
 
 5.times do
-  sample :drum_bass_soft
+  sample :bd_haus
   sleep 1
 end
 
+use_bpm 128
+
+live_loop :drums do
+  5.times do
+    5.times do
+      sample drum_beat_sample, beat_stretch: 8
+      sleep 8
+    end
+    stop
+  end
+end
+sleep 2
 
 
-
+live_loop :bell do
+  30.times do
+    sample :drum_cymbal_closed
+    sleep 0.5
+    sample :drum_cymbal_pedal
+    sleep 0.5
+  end
+  stop
+end
 
 
 
